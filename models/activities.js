5b1100d51a8315c2fe654d2623c707d68c79bfa9
Bascur <bascur62@gmail.com>
2021-11-23 12:07:11 -0300

    activities model & migration

---
+++ models/activities.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+    Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class activities extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    };
+    activities.init({
+        name: DataTypes.STRING,
+        content: DataTypes.STRING,
+        image: DataTypes.STRING
+    }, {
+        sequelize,
+        modelName: 'activities',
+        paranoid: true
+    });
+    return activities;
+};
