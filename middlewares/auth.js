c3b48077619d670ff713d332416d8140185af209
AgustinTafura <agustintafura@hotmail.com>
2021-11-24 17:17:28 -0300

    Auth : add isAdmin middleware funcionality

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,7 +1,22 @@
-const isAdmin = (req,res,next) => {
-    
+const roleService = require('../services/roles') 
 
-    next()
+const isAdmin = async (req,res,next) => {
+    
+    const token = req.header('auth-token')
+    const adminRoleId = await roleService.getAdminRoleId() // should be include this funcionality in services/roles ?
+
+    if (!token) return res.status(401).json({ error: 'Access denied' })
+
+    try {
+        const user = decodeToken(token)
+
+        if (user.roleId !== adminRoleId) return res.status(401).json({ error: 'Access denied' })
+        next()
+        
+    } catch (error) {
+        res.status(400).json({error: 'token no es válido'})
+    }
+
 }
 
 module.exports = {
