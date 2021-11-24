dd4f2fc5f17b0dc864dcbef867eb5c3dd447cfd1
Sebastián Galván <sebastiangalvan.ar@gmail.com>
2021-11-24 14:34:04 -0300

    Create Model, Migration, and Base Controller from Categories

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,14 +1,14 @@
-const Categories = require('../services/categories')
+const Categories = require('../services/categories');
 
 const getAll = async(req, res, next)=>{
     try {
-        const categories = await Categories.getAll()
-        res.status(200).json(categories)
+        const categories = await Categories.getAll();
+        res.status(200).json(categories);
     } catch (e) {
-        next(e)
+        next(e);
     }
 }
 
 module.exports = {
-    getAll
+    getAll,
 }
