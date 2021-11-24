64bf29943694a675b877fb35a3119b21e33b5ce6
AgustinTafura <agustintafura@hotmail.com>
2021-11-24 14:02:39 -0300

    News: create Route and Model

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ const logger = require('morgan');
 const cors = require('cors')
 require('dotenv').config()
 
-const indexRouter = require('./routes/index');
+// const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const newsRouter = require('./routes/news');
 
 const app = express();
 app.use(cors())
@@ -22,8 +23,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+// app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/news', newsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ services/news.js
@@ -0,0 +1,33 @@
+// const { Op, Sequelize } = require("sequelize");
+const {News} = require('../models/index')
+
+
+const getAll = async (req, res, next) => {
+
+}
+
+const create = async (req, res, next) => {
+    
+}
+
+const getById = async (req, res, next) => {
+
+}
+
+const update = async (req, res, next) => {
+
+}
+
+const remove = async (req, res, next) => {
+
+}
+
+
+
+module.exports = {
+    getAll, 
+    getById, 
+    create,
+    update,
+    remove
+}
