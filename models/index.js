1ba393980d4c3720570612f90902f8c456d56764
fgarancini <f.garancini15@gmail.com>
2021-11-24 22:32:42 -0300

    delete comment

--- models/index.js
+++ models/index.js
@@ -28,6 +28,5 @@ Object.keys(db).forEach(modelName => {
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-// db.Users = require('./users')(db.sequelize,Sequelize.DataTypes);
 
 module.exports = db;
