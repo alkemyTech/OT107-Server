237ff74c6e2e3be1a767f0907751b12caa4b5d26
fgarancini <f.garancini15@gmail.com>
2021-11-26 18:20:38 -0300

    seed users

--- README.md
+++ README.md
@@ -26,3 +26,151 @@ npx sequelize-cli db:seed:all
 ``` bash
 npm start
 ```
+
+
+## Usuarios
+
+```bash
+[
+  {
+    "firstName": "Agustin",
+    "lastName": "Tafura",
+    "email": "agustin_tafura@test.com",
+    "password": "123456789_t",
+    "roleId": 1
+  },
+  {
+    "firstName": "Andrea",
+    "lastName": "Maccan",
+    "email": "andrea_maccan@test.com",
+    "password": "123456789_a",
+    "roleId": 1
+  },
+  {
+    "firstName": "Ezequiel",
+    "lastName": "Astrada",
+    "email": "ezequiel_astrada@test.com",
+    "password": "123456789_e",
+    "roleId": 1
+  },
+  {
+    "firstName": "Franco",
+    "lastName": "Garancini",
+    "email": "franco_garancini@test.com",
+    "password": "123456789_f",
+    "roleId": 1
+  },
+  {
+    "firstName": "joaquin",
+    "lastName": "Bascur",
+    "email": "bascur@test.com",
+    "password": "123456789_j",
+    "roleId": 1
+  },
+  {
+    "firstName": "Juan Pablo",
+    "lastName": "Choter",
+    "email": "juantablo_ch@test.com",
+    "password": "123456789_j",
+    "roleId": 1
+  },
+  {
+    "firstName": "Matias",
+    "lastName": "Dalceggio",
+    "email": "matias_dalceggio@test.com",
+    "password": "123456789_m",
+    "roleId": 1
+  },
+  {
+    "firstName": "Sebastian",
+    "lastName": "Galvan",
+    "email": "seba_galvan@test.com",
+    "password": "123456789_s",
+    "roleId": 1
+  },
+  {
+    "firstName": "Manuel",
+    "lastName": "Francisco",
+    "email": "manu_f@test.com",
+    "password": "123456789_m",
+    "roleId": 1
+  },
+  {
+    "firstName": "Usuario_dePrueba",
+    "lastName": "Demo",
+    "email": "test@test.com",
+    "password": "5678",
+    "roleId": 1
+  },
+  {
+    "firstName": "Usuario_dePrueba",
+    "lastName": "Demo",
+    "email": "test@test.com",
+    "password": "5678",
+    "roleId": 1
+  },
+  {
+    "firstName": "Usuario_dePrueba",
+    "lastName": "Demo",
+    "email": "test@test.com",
+    "password": "5678",
+    "roleId": 1
+  },
+  {
+    "firstName": "Elon",
+    "lastName": "Musk",
+    "email": "e_musk@test.com",
+    "password": "dogecoin",
+    "roleId": 2
+  },
+  {
+    "firstName": "Steeve",
+    "lastName": "Jobs",
+    "email": "steeve_j@test.com",
+    "password": "i<3microsoft",
+    "roleId": 2
+  },
+  {
+    "firstName": "Mark",
+    "lastName": "Zuckerberg",
+    "email": "facebook@facebook.com",
+    "password": "metafacebook",
+    "roleId": 2
+  },
+  {
+    "firstName": "Satoshi",
+    "lastName": "Naka",
+    "email": "btc@test.com",
+    "password": "shibamoon",
+    "roleId": 2
+  },
+  {
+    "firstName": "Usuario_dePrueba",
+    "lastName": "Demo",
+    "email": "test@test.com",
+    "password": "5678",
+    "roleId": 2
+  },
+  {
+    "firstName": "Usuario_dePrueba",
+    "lastName": "Demo",
+    "email": "test@test.com",
+    "password": "5678",
+    "roleId": 2
+  },
+  {
+    "firstName": "Usuario_dePrueba",
+    "lastName": "Demo",
+    "email": "test@test.com",
+    "password": "5678",
+    "roleId": 2
+  },
+  {
+    "firstName": "Usuario_dePrueba",
+    "lastName": "Demo",
+    "email": "test@test.com",
+    "password": "5678",
+    "roleId": 2
+  }
+]
+```
--- seeders/20201109013159-create-demo-user.js
+++ seeders/20201109013159-create-demo-user.js
@@ -3,12 +3,202 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Users', [{
-      firstName: 'Usuario',
+      firstName: 'Agustin',
+      lastName: 'Tafura',
+      email: 'agustin_tafura@test.com',
+      // Important: Password not encrypted yet! 
+      password: '123456789_t',
+      roleId: 1,
+      image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+      createdAt: new Date,
+      updatedAt: new Date
+    },{
+      firstName: 'Andrea',
+      lastName: 'Maccan',
+      email: 'andrea_maccan@test.com',
+      // Important: Password not encrypted yet! 
+      password: '123456789_a',
+      roleId: 1,
+      image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+      createdAt: new Date,
+      updatedAt: new Date
+    },{
+      firstName: 'Ezequiel',
+      lastName: 'Astrada',
+      email: 'ezequiel_astrada@test.com',
+      // Important: Password not encrypted yet! 
+      password: '123456789_e',
+      roleId: 1,
+      image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+      createdAt: new Date,
+      updatedAt: new Date
+    },{
+      firstName: 'Franco',
+      lastName: 'Garancini',
+      email: 'franco_garancini@test.com',
+      // Important: Password not encrypted yet! 
+      password: '123456789_f',
+      roleId: 1,
+      image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+      createdAt: new Date,
+      updatedAt: new Date
+    },{
+      firstName: 'joaquin',
+      lastName: 'Bascur',
+      email: 'bascur@test.com',
+      // Important: Password not encrypted yet! 
+      password: '123456789_j',
+      roleId: 1,
+      image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+      createdAt: new Date,
+      updatedAt: new Date
+    },{
+      firstName: 'Juan Pablo',
+      lastName: 'Choter',
+      email: 'juantablo_ch@test.com',
+      // Important: Password not encrypted yet! 
+      password: '123456789_j',
+      roleId: 1,
+      image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+      createdAt: new Date,
+      updatedAt: new Date
+    },{
+      firstName: 'Matias',
+      lastName: 'Dalceggio',
+      email: 'matias_dalceggio@test.com',
+      // Important: Password not encrypted yet! 
+      password: '123456789_m',
+      roleId: 1,
+      image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+      createdAt: new Date,
+      updatedAt: new Date
+    },{
+      firstName: 'Sebastian',
+      lastName: 'Galvan',
+      email: 'seba_galvan@test.com',
+      // Important: Password not encrypted yet! 
+      password: '123456789_s',
+      roleId: 1,
+      image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+      createdAt: new Date,
+      updatedAt: new Date
+    },{
+      firstName: 'Manuel',
+      lastName: 'Francisco',
+      email: 'manu_f@test.com',
+      // Important: Password not encrypted yet! 
+      password: '123456789_m',
+      roleId: 1,
+      image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+      createdAt: new Date,
+      updatedAt: new Date
+    },{
+      firstName: 'Usuario_dePrueba',
       lastName: 'Demo',
       email: 'test@test.com',
       // Important: Password not encrypted yet! 
-      password: '1234',
+      password: '5678',
       roleId: 1,
+      image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+      createdAt: new Date,
+      updatedAt: new Date
+    },{
+      firstName: 'Usuario_dePrueba',
+      lastName: 'Demo',
+      email: 'test@test.com',
+      // Important: Password not encrypted yet! 
+      password: '5678',
+      roleId: 1,
+      image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+      createdAt: new Date,
+      updatedAt: new Date
+    },{
+      firstName: 'Usuario_dePrueba',
+      lastName: 'Demo',
+      email: 'test@test.com',
+      // Important: Password not encrypted yet! 
+      password: '5678',
+      roleId: 1,
+      image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+      createdAt: new Date,
+      updatedAt: new Date
+    },{
+      firstName: 'Elon',
+      lastName: 'Musk',
+      email: 'e_musk@test.com',
+      // Important: Password not encrypted yet! 
+      password: 'dogecoin',
+      roleId: 2,
+      image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+      createdAt: new Date,
+      updatedAt: new Date
+    },{
+      firstName: 'Steeve',
+      lastName: 'Jobs',
+      email: 'steeve_j@test.com',
+      // Important: Password not encrypted yet! 
+      password: 'i<3microsoft',
+      roleId: 2,
+      image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+      createdAt: new Date,
+      updatedAt: new Date
+    },{
+      firstName: 'Mark',
+      lastName: 'Zuckerberg',
+      email: 'facebook@facebook.com',
+      // Important: Password not encrypted yet! 
+      password: 'metafacebook',
+      roleId: 2,
+      image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+      createdAt: new Date,
+      updatedAt: new Date
+    },{
+      firstName: 'Satoshi',
+      lastName: 'Naka',
+      email: 'btc@test.com',
+      // Important: Password not encrypted yet! 
+      password: 'shibamoon',
+      roleId: 2,
+      image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+      createdAt: new Date,
+      updatedAt: new Date
+    },{
+      firstName: 'Usuario_dePrueba',
+      lastName: 'Demo',
+      email: 'test@test.com',
+      // Important: Password not encrypted yet! 
+      password: '5678',
+      roleId: 2,
+      image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+      createdAt: new Date,
+      updatedAt: new Date
+    },{
+      firstName: 'Usuario_dePrueba',
+      lastName: 'Demo',
+      email: 'test@test.com',
+      // Important: Password not encrypted yet! 
+      password: '5678',
+      roleId: 2,
+      image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+      createdAt: new Date,
+      updatedAt: new Date
+    },{
+      firstName: 'Usuario_dePrueba',
+      lastName: 'Demo',
+      email: 'test@test.com',
+      // Important: Password not encrypted yet! 
+      password: '5678',
+      roleId: 2,
+      image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+      createdAt: new Date,
+      updatedAt: new Date
+    },{
+      firstName: 'Usuario_dePrueba',
+      lastName: 'Demo',
+      email: 'test@test.com',
+      // Important: Password not encrypted yet! 
+      password: '5678',
+      roleId: 2,
       image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
       createdAt: new Date,
       updatedAt: new Date
