becfbb09d00393f2163190dd9c1ac59431155229
juanpabloch <63684218+juanpabloch@users.noreply.github.com>
2021-11-25 22:48:58 -0300

    add endpoint

--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,6 @@ const organizationRoutes = require('./organizations');
 
 router.use('/users', userRoutes);
 router.use('/testimonials', testimonialsRoutes);
+router.use('/organizations', organizationRoutes);
 
 module.exports = router;
