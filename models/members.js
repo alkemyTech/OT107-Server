4ac27365ab1386b2bebea45625e853a309858194
Matias Dalceggio <matiasdalceggio@gmail.com>
2021-11-26 20:44:08 -0300

    id problem solved model members

--- models/members.js
+++ models/members.js
@@ -7,20 +7,47 @@ module.exports = (sequelize, DataTypes) => {
     }
   };
   Members.init({  
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
      
-    name: DataTypes.STRING,
-    facebookUrl: DataTypes.STRING,
-    instagramUrl: DataTypes.STRING,
-    linkedinUrl: DataTypes.STRING,
-    image: DataTypes.STRING,
-    description: DataTypes.STRING,
+    name:{
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    facebookUrl: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    instagramUrl:{
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    linkedinUrl: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
     createdAt: DataTypes.DATE,
     updatedAt: DataTypes.DATE,
     deletedAt: DataTypes.DATE
   }, {
     sequelize,
     modelName: 'Members',
+    tableName: "members",
     paranoid: true
   });
   return Members;
 };
+
+
