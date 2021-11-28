4da80e3b9636ec6a81c4ee3120be5e0094f972aa
Sebastián Galván <47829817+SebasGalvan@users.noreply.github.com>
2021-11-28 16:33:09 -0300

    OT107-34 - Middleware de Ownership. (#23)
    * OT107-34 -  [ADD] Middleware de Ownership
    
    * OT107-34 -Middleware de Ownership
    
    * OT107-34 -Middleware de Ownership
    
    * OT107-34 -Middleware de Ownership
    
    * OT107-34 -Middleware de Ownership
    
    * OT107-34 -Middleware de Ownership -

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,7 +1,30 @@
+const userService =  require('../services/users');
+const rolesService =  require('../services/roles');
+const auth = require('../modules/auth');
 
-/* eslint-disable consistent-return */
-const rolesServices = require('../services/roles');
-const auth = require('../module/auth');
+const  isOwnUser = async (req, res, next) => {
+
+	try {
+
+		const userId = req.body.id;
+		const userToken = req.headers['authorization'];
+		const usuarioToken = auth.decodeToken(userToken);
+		const userTokenId = usuarioToken.id;
+		
+		if(userTokenId){
+			if (Number.parseInt(userId) === userTokenId) return next();
+		}
+		const user = await userService.getById(userTokenId);
+		if(user){
+			const adminUser = await rolesService.getByName('Admin');
+			if (user.dataValues.roleId === adminUser.id) return next();
+		}	
+		const e = new Error('not authorized');
+		throw e;
+	} catch (e) {
+		next(e);
+	}
+};
 
 const isAdmin = async (req, res, next) => {
   const token = req.header('auth-token');
@@ -20,6 +43,8 @@ const isAdmin = async (req, res, next) => {
 };
 
 module.exports = {
-  isAdmin
+  isAdmin,
+  isOwnUser
 
 };
+
--- repositories/roles.js
+++ repositories/roles.js
@@ -1,10 +1,23 @@
-const Roles = require("../models/roles");
+const Roles = require('../models/roles');
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
 const getAll = async () => {
-  const data = await Roles.findAll();
-  return data;
+	const data = await Roles.findAll();
+	return data;
+};
+
+const getByName = async (name) => {
+	const data = await Roles.findOne({ where: { name : {[Op.substring]:name}}});
+	const roleInfo = {
+		id : data.dataValues.id,
+		name : data.dataValues.name,
+		content : data.dataValues.content
+	};
+	return roleInfo;
 };
 
 module.exports = {
-    getAll,
+	getAll,
+	getByName,
 };
