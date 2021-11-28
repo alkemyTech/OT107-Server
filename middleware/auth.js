b089f2893d2112e5f20f58f56bce8b8045a53b80
Joaquin Bascur <89546054+Bascur@users.noreply.github.com>
2021-11-27 22:53:21 -0300

    Asked changes

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,26 +1,21 @@
 const { check, validationResult } = require('express-validator');
 
-
-
-const router = require('express').Router();
-
-const registerUsersValidator = router.post('/', [
+const registerInputValidation = [
     check('firstName', 'First Name Required').not().isEmpty(),
     check('lastName', 'Last Name Required').not().isEmpty(),
     check('email', 'Invalid Email').isEmail(),
     check('password', 'Invalid Password').not().isEmpty()
-], async(req, res, next) => {
+ , 
+    (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ error: errors.array() });
     }
-
     next();
+ }
+];
 
-});
 
 module.exports = {
-    registerUsersValidator
+    registerInputValidation
 }
-
-
