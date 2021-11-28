e1cd6444cbf59655973ca563cc00e76d3f81dfdf
Joaquin Bascur <89546054+Bascur@users.noreply.github.com>
2021-11-28 20:38:44 -0300

    Ot107 105 (#20)
    * activities model & migration
    
    * soft delete
    
    * Activities endpoint
    
    * Asume middleware
    
    * Asked changes
    
    * Delete package-lock.json
    
    Co-authored-by: manuelfrancisco <franciscomanueljose@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,11 @@
 const express = require('express');
+
+const router = express.Router();
+const activitiesRouter = require('./activities');
+
+router.use('/activities', activitiesRouter);
+
+module.exports = router;
 const router = express.Router();
 const activitiesRouter = require('./activities');
 const registerRouter = require('./auth');
