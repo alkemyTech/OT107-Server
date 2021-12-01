1f0cf6cbcf08be7445edcd6b0cd8ee207506bd72
AgustinTafura <agustintafura@hotmail.com>
2021-11-30 23:58:44 -0300

    validation: add news categoryId from DB by default

--- services/news.js
+++ services/news.js
@@ -1,12 +1,20 @@
 const newsRepository = require('../repositories/news');
+const categoriesRepository = require('../repositories/categories');
 
 const getAll = async () => {
   const news = await newsRepository.getAll();
   return news;
 };
 
-const create = async () => {
+const create = async (data) => {
+  const validData = data;
+  // assignment news categoryId from Categories
+  const newsCategory = await categoriesRepository.getByName('news');
+  validData.categoryId = newsCategory.id;
 
+  const newNews = await newsRepository.create(validData);
+
+  return newNews;
 };
 
 const getById = async () => {
