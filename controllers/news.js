15f5cab9d3a21dacae62fad38894a895e22f9863
AgustinTafura <agustintafura@hotmail.com>
2021-12-01 13:55:10 -0300

    remove validation

--- controllers/news.js
+++ controllers/news.js
@@ -4,7 +4,6 @@ const getAll = async (req, res, next) => {
   try {
     const news = await newsService.getAll();
 
-    if (!news) throw new Error('bad request');
     res.status(200).json(news);
   } catch (e) {
     next(e);
@@ -14,53 +13,24 @@ const getAll = async (req, res, next) => {
 const create = async (req, res, next) => {
   try {
     const data = req.body;
-    const news = await newsService.create(data);
+    const novelty = await newsService.create(data);
 
-    if (!news) throw new Error('bad request');
-    res.sendStatus(204);
+    res.status(200).json(novelty);
   } catch (e) {
     next(e);
   }
 };
 
 const getById = async (req, res, next) => {
-  try {
-    const { id } = req.params;
 
-    const news = id ? await newsService.getById(id) : null;
-
-    if (!news) throw new Error('bad request');
-    res.status(200).json(news);
-  } catch (e) {
-    next(e);
-  }
 };
 
 const update = async (req, res, next) => {
-  try {
-    const data = req.body;
-    const { id } = req.params;
 
-    const news = id ? await newsService.update(id, data) : null;
-
-    if (!news) throw new Error('bad request');
-    res.sendStatus(204);
-  } catch (e) {
-    next(e);
-  }
 };
 
 const remove = async (req, res, next) => {
-  try {
-    const { id } = req.params;
 
-    const news = id ? await newsService.remove(id) : null;
-
-    if (!news) throw new Error('bad request');
-    res.sendStatus(204);
-  } catch (e) {
-    next(e);
-  }
 };
 
 module.exports = {
