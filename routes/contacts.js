b10ed59664461b9c48f615590104b3121f3d6363
astradae <astradaezequiel.tpi@gmail.com>
2021-12-01 20:27:42 -0300

    OT107-56 POST /contacts - Endpoint para almacenar contactos

--- routes/contacts.js
+++ routes/contacts.js
@@ -3,8 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 const contactsController = require('../controllers/contacts');
+const contactsMiddleware = require('../middlewares/contacts');
 const authMiddleware = require('../middlewares/auth');
 
-router.get('/', authMiddleware.isAdmin, contactsController.getAll);
+router.post('/', contactsMiddleware.contactsInputValidation, authMiddleware.isAuth,contactsController.create);
 
 module.exports = router;
