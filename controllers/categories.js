b5cbbecbdbbe6be0621a241886924c48f3b715a6
Sebastián Galván <sebastiangalvan.ar@gmail.com>
2021-12-01 10:46:13 -0300

    OT107-43 - Endpoint Creación Categorías

--- controllers/categories.js
+++ controllers/categories.js
@@ -11,10 +11,10 @@ const getAll = async (req, res, next) => {
 
 const create = async (req, res, next) => {
   try {
-    const response = await categoriesService.create(req.body);
+    const category = await categoriesService.create(req.body);
     return res.status(200).json({
       msg: `Category created: ${req.body.name}`,
-      Category: response
+      category
     });
   } catch (error) {
     next(error);
