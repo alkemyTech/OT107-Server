7251130878996e9d043b311f68d8d648bf0f66d0
AgustinTafura <agustintafura@hotmail.com>
2021-12-01 14:00:28 -0300

    conflict resolved

--- routes/users.js
+++ routes/users.js
@@ -2,13 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 const usersController = require('../controllers/users');
-<<<<<<< HEAD
-/* GET users listing. */
-router.get('/', usersController.getAll);
-=======
 const authMiddleware = require('../middlewares/auth');
 /* GET users listing. */
 router.get('/', authMiddleware.isAdmin, usersController.getAll);
->>>>>>> fac61fb9c64a1af40c561578aa0b08022cebe48a
 
 module.exports = router;
