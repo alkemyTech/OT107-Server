b29b62b24cc6315c3d3cd639c531868b2a7c8deb
AgustinTafura <agustintafura@hotmail.com>
2021-11-30 21:39:31 -0300

    add newsInputValidator, isImage

--- controllers/news.js
+++ controllers/news.js
@@ -1,105 +1,87 @@
-const newsService  = require('../services/news')
+const newsService = require('../services/news');
 
 const getAll = async (req, res, next) => {
-    try {
-        const news = await newsService.getAll()
+  try {
+    const news = await newsService.getAll();
 
-
-        if (news){
-            res.status(200).json(news)
-        } else {
-            throw new Error('bad request')
-        }
-
-    } catch (e) {
-        next(e)
+    if (news) {
+      res.status(200).json(news);
+    } else {
+      throw new Error('bad request');
     }
-}
+  } catch (e) {
+    next(e);
+  }
+};
 
 const create = async (req, res, next) => {
-    
-    
-    try {
-        const data = req.body
-        const news = await newsService.create(data)
+  try {
+    const data = req.body;
+    const news = await newsService.create(data);
 
-
-        if (news){
-            res.sendStatus(204)
-        } else {
-            throw new Error('bad request')
-        }
-
-    } catch (e) {
-        next(e)
+    if (news) {
+      res.sendStatus(204);
+    } else {
+      throw new Error('bad request');
     }
-}
+  } catch (e) {
+    next(e);
+  }
+};
 
 const getById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
 
-    try {
+    const news = id ? await newsService.getById(id) : null;
 
-        const id = req.params.id
-
-       const news = id ? await newsService.getById(id) : null
-
-
-        if (news){
-            res.status(200).json(news)
-        } else {
-            throw new Error('bad request')
-        }
-
-    } catch (e) {
-        next(e)
+    if (news) {
+      res.status(200).json(news);
+    } else {
+      throw new Error('bad request');
     }
-}
+  } catch (e) {
+    next(e);
+  }
+};
 
 const update = async (req, res, next) => {
+  try {
+    const data = req.body;
+    const { id } = req.params;
 
-    try {
-        const data = req.body
-        const id = req.params.id
+    const news = id ? await newsService.update(id, data) : null;
 
-        const news = id ? await newsService.update(id, data) : null
-
-
-        if (news){
-            res.sendStatus(204)
-        } else {
-            throw new Error('bad request')
-        }
-
-    } catch (e) {
-        next(e)
+    if (news) {
+      res.sendStatus(204);
+    } else {
+      throw new Error('bad request');
     }
-}
+  } catch (e) {
+    next(e);
+  }
+};
 
 const remove = async (req, res, next) => {
+  try {
+    const { id } = req.params;
 
-    try {
-        const id = req.params.id
+    const news = id ? await newsService.remove(id) : null;
 
-        const news = id ? await newsService.remove(id) : null
-
-
-        if (news){
-            res.sendStatus(204)
-        } else {
-            throw new Error('bad request')
-        }
-
-    } catch (e) {
-        next(e)
+    if (news) {
+      res.sendStatus(204);
+    } else {
+      throw new Error('bad request');
     }
-}
-
-
+  } catch (e) {
+    next(e);
+  }
+};
 
 module.exports = {
-    getAll, 
-    getById, 
-    create,
-    update,
-    remove
-}
+  getAll,
+  getById,
+  create,
+  update,
+  remove
+};
--- routes/news.js
+++ routes/news.js
@@ -3,9 +3,10 @@ const express = require('express');
 const router = express.Router();
 const newsController = require('../controllers/news');
 const authMiddleware = require('../middlewares/auth');
+const newsMiddleware = require('../middlewares/news');
 
 router.route('/')
   .get(authMiddleware.isAdmin, newsController.getAll)
-  .post(authMiddleware.isAdmin, newsController.create);
+  .post(authMiddleware.isAdmin, newsMiddleware.newsInputValidation, newsController.create);
 
 module.exports = router;
