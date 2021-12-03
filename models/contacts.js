a82bbb11fb3d1f69de4cdd9f9073febc07036f8f
Astrada Ezequiel <82248297+as-ez@users.noreply.github.com>
2021-12-02 22:05:56 -0300

    fix - migracion y modelo de contacts (#58)

--- models/contacts.js
+++ models/contacts.js
@@ -1,7 +1,5 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Contacts extends Model {
     /**
@@ -14,36 +12,14 @@ module.exports = (sequelize, DataTypes) => {
     }
   };
   Contacts.init({
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    phone: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    message: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    deletedAt: {
-      type: DataTypes.DATE,
-      allowNull: false
-    }
+    name: DataTypes.STRING,
+    phone: DataTypes.STRING,
+    email: DataTypes.STRING,
+    message: DataTypes.STRING,
+    deleteAt: DataTypes.DATE
   }, {
     sequelize,
     modelName: 'Contacts',
-    tableName: 'contacts'
   });
   return Contacts;
 };
