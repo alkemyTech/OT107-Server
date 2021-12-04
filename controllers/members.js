bbacf70556005276e89b846e41f0071a9a0a6d3a
Matías Ángel Dalceggio <83846321+MatiDalce@users.noreply.github.com>
2021-12-04 11:17:38 -0300

    Ot107 74 (#69)
    * seed users
    
    * fix != emails
    
    * fix + readme JSON
    
    * OT107-26 - Middleware para usuarios autenticados (#34)
    
    * OT107-20 First Commit (#7)
    
    * OT107-104 - Testimonials Endpoints (#6)
    
    * OT107-104 - Testimonials Endpoints
    * Access for isAdmin Role
    * Code cleaned
    
    * Ot107-32 (#10)
    
    * logic for JWT
    
    * refactor jwt, moved to modules folder
    
    * package.json with jsonwebtoken
    
    * OT107-26 - Middleware para usuarios autenticados
    
    * Add getById service and imports review
    
    * Change from findOne to findByPk
    
    Co-authored-by: Juan Pablo Choternasty <63684218+juanpabloch@users.noreply.github.com>
    
    * fix roles 10/10
    
    * demo fixes
    
    * correccion rutas, repo/rol, service/contact, service/news
    
    * fix
    
    * Sebastian :  sin confictos no toque codigo de otro
    controllers/categories.js
    repositories/roles.js
    repositories/categories.js
    repositories/users.js
    routes/categories.js
    routes/index.js
    services/categories.js
    services/roles.js
    services/users.js
    ****
    middleware/auth.js : habia un require que se repetia el de  /services/users  lo quite
    
    * categories: correccion nombre archivo de migracion
    
    * login ok
    
    * members folder organization
    
    * members route
    
    * Algunos arreglos de comillas dobles + .env.example + seeder de Users
    
    * test and review code, little changes
    
    * Members response, models and seeder testimonial changes
    
    * Transformación de datos -  [Roles] - Del repository a service.
    
    * En routes/activities.js cambiar nombre del middewarer - >  authMiddleware
    
    * Corrección de un try/catch en el servicio news.js y cosas del eslint
    
    * Estandarizar token
    
    * Actualizado con Bearer Token
    
    * Refactored to auth
    
    * asked changes
    
    * Change isOwner middleware, delete validPassword
    
    * Asked changes
    
    * create new member
    
    * validation members create
    
    * middleware create
    
    * codoowners file
    
    * is a string validation and replace createNew for create function names
    
    * eslint format
    
    * 74
    
    Co-authored-by: manuelfrancisco <franciscomanueljose@gmail.com>
    Co-authored-by: fgarancini <f.garancini15@gmail.com>
    Co-authored-by: Andrea <78446973+acmaccan@users.noreply.github.com>
    Co-authored-by: Juan Pablo Choternasty <63684218+juanpabloch@users.noreply.github.com>
    Co-authored-by: Bascur <bascur62@gmail.com>
    Co-authored-by: AgustinTafura <agustintafura@hotmail.com>
    Co-authored-by: Sebastián Galván <sebastiangalvan.ar@gmail.com>
    Co-authored-by: astradae <astradaezequiel.tpi@gmail.com>
    Co-authored-by: Andrea Maccan <acmaccan@gmail.com>

--- controllers/members.js
+++ controllers/members.js
@@ -1,22 +1,24 @@
 const membersService = require('../services/members');
 
 const getAll = async (req, res, next) => {
-	try {
-		const members = await membersService.getAll();
-		res.status(200).json(members);
-	}catch(e){ next(e)
-	}
+  try {
+    const members = await membersService.getAll();
+    res.status(200).json(members);
+  } catch (e) {
+    next(e);
+  }
 };
 
-const create = async (req,res,next) => { 
-
-	try{const members = await  membersService.create(req.body); 
-	res.status(200).json(members)}catch(e){	
-		next(e)
-	}
-}
+const create = async (req, res, next) => {
+  try {
+    const members = await membersService.create(req.body);
+    res.status(200).json(members);
+  } catch (e) {
+    next(e);
+  }
+};
 
 module.exports = {
-	getAll,
-	create
+  getAll,
+  create
 };
