d02844eba7692ab7d23792aad741ce280fbf6b38
juanpabloch <63684218+juanpabloch@users.noreply.github.com>
2021-12-04 02:08:29 -0300

    slide getById controller

--- controllers/slides.js
+++ controllers/slides.js
@@ -9,6 +9,17 @@ const getAll = async (req, res, next) => {
   }
 };
 
+const getById = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const slide = await slidesServices.getById(id);
+    res.status(200).json(slide);
+  } catch (e) {
+    next(e);
+  }
+};
+
 module.exports = {
-  getAll
+  getAll,
+  getById
 };
