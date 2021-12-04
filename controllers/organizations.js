571647943fb960899d2a580dc8045175a623121b
Juan Pablo Choternasty <63684218+juanpabloch@users.noreply.github.com>
2021-12-04 17:04:14 -0300

    OT107-38 organization update controller and seeder (#73)
    * organization update controller and seeder
    
    * change controller responsibilities to service

--- controllers/organizations.js
+++ controllers/organizations.js
@@ -10,6 +10,17 @@ const getOrganizationPublic = async (req, res, next) => {
   }
 };
 
+const updateOrganization = async (req, res, next) => {
+  try {
+    const id = configuration.idOrganization;
+    const data = await organizationService.update(id, req.body);
+    res.status(200).json(data);
+  } catch (e) {
+    next(e);
+  }
+};
+
 module.exports = {
-  getOrganizationPublic
+  getOrganizationPublic,
+  updateOrganization
 };
--- routes/organizations.js
+++ routes/organizations.js
@@ -3,7 +3,10 @@ const express = require('express');
 const router = express.Router();
 
 const organizationControllers = require('../controllers/organizations');
+const authMiddleware = require('../middlewares/auth');
+const updateValidations = require('../middlewares/organizations');
 
 router.get('/public', organizationControllers.getOrganizationPublic);
+router.put('/public', [authMiddleware.isAdmin, updateValidations.organizationInputValidation], organizationControllers.updateOrganization);
 
 module.exports = router;
