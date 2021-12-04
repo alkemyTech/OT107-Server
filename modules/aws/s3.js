3ad794c67a6dfad945bb21c1cc859836e1b7eaf9
Agustin Tafura <38706816+AgustinTafura@users.noreply.github.com>
2021-12-04 00:00:51 -0300

    Ot107 39b (#63)
    * add AWS
    
    * add aws/s3 config + upload functionality with UUID

---
+++ modules/aws/s3.js
@@ -0,0 +1,32 @@
+const S3 = require('aws-sdk/clients/s3');
+const fs = require('fs');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+
+const region = process.env.AWS_REGION;
+const accessKeyId = process.env.AWS_ACCESS_KEY;
+const secretAccessKey = process.env.AWS_SECRET_KEY;
+const bucket = process.env.AWS_BUCKET;
+
+const s3 = new S3({
+  region,
+  accessKeyId,
+  secretAccessKey
+});
+
+const uploadToBucket = async (file, awsBucketName) => {
+  const stream = fs.createReadStream(file.tempFilePath || file.path);
+  const extension = path.extname(file.name || file.fieldname || file.originalname).toLowerCase();
+  const params = {
+    Bucket: awsBucketName || bucket,
+    Key: uuidv4() + extension,
+    Body: stream
+  };
+  const fileUpload = await s3.upload(params).promise();
+
+  return fileUpload;
+};
+
+module.exports = {
+  uploadToBucket
+};
