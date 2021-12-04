849209caf4971ff70bbba6c29e877a273f4b5a59
juanpabloch <63684218+juanpabloch@users.noreply.github.com>
2021-12-04 10:56:59 -0300

    add isAdmin middelware to route

--- routes/slides.js
+++ routes/slides.js
@@ -5,6 +5,6 @@ const authMiddleware = require('../middlewares/auth');
 const router = express.Router();
 
 router.get('/', authMiddleware.isAdmin, slidesControllers.getAll);
-router.get('/:id', slidesControllers.getById);
+router.get('/:id', authMiddleware.isAdmin, slidesControllers.getById);
 
 module.exports = router;
