4cdb87204950bf1a81a03248f9f16a690f778323
Astrada Ezequiel <82248297+as-ez@users.noreply.github.com>
2021-12-05 12:50:11 -0300

    OT107-82 // Endpoint Actualización Comentarios PUT /comments/:id (#86)

--- repositories/comments.js
+++ repositories/comments.js
@@ -3,9 +3,14 @@ const Models = require('../models');
 const getAll = async () => {
   const consult = await Models.Comments.findAll({
     attributes: ['body'],
-    order: ['createdAt', 'ASC']
+    order: [['createdAt', 'ASC']]
   });
   return consult;
+};
+
+const getById = async (id) => {
+  const comment = await Models.Comments.findByPk(id);
+  return comment;
 };
 
 const create = async (comment) => {
@@ -13,7 +18,14 @@ const create = async (comment) => {
   return response;
 };
 
+const update = async (id, data) => {
+  const comment = await Models.Comments.update(data, { where: { id } });
+  return comment;
+};
+
 module.exports = {
   getAll,
   create,
+  getById,
+  update
 };
