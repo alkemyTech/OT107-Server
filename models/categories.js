5de51700b1a9e906badfea1acd1f4a6b87793396
AgustinTafura <agustintafura@hotmail.com>
2021-12-08 19:58:43 -0300

    ESLint correction

--- models/categories.js
+++ models/categories.js
@@ -1,5 +1,5 @@
-'use strict';
-const {Model} = require('sequelize');
+const { Model } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class Categories extends Model {
     /**
@@ -10,12 +10,12 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
-  };
+  }
   Categories.init({
     name: DataTypes.STRING,
     description: DataTypes.STRING,
     image: DataTypes.STRING,
-    deletedAt : DataTypes.DATE,
+    deletedAt: DataTypes.DATE,
   }, {
     sequelize,
     modelName: 'Categories',
