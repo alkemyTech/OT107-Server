10a1b5c975fc65fe9aa42007d7ffa66b5b5a6d65
Sebastián Galván <sebastiangalvan.ar@gmail.com>
2021-12-09 14:45:19 -0300

    OT107-106 Agregar módulo reutilizable de paginación

---
+++ modules/paginationModule.js
@@ -0,0 +1,34 @@
+const pagination = (limit, count, req, model) => {
+  const lastPage = Math.ceil(count / limit);
+  const page = Number(req.page);
+  let previousPageUrl = null;
+  let nextPageUrl = null;
+
+  if (page > lastPage) {
+    const error = new Error(`The requested page is greater than the last page. [1-${lastPage}]`);
+    error.status = 400;
+    throw error;
+  }
+  const urlBase = `${req.protocol}://${req.host}/${model}`;
+
+  if (page > 1) {
+    previousPageUrl = `${urlBase}?page=${page - 1}`;
+  }
+  if (page !== lastPage) {
+    nextPageUrl = `${urlBase}?page=${page + 1}`;
+  }
+  const offset = (page - 1) * limit;
+
+  const paginationData = {
+    lastPage,
+    offset,
+    previousPageUrl,
+    nextPageUrl
+  };
+
+  return paginationData;
+};
+
+module.exports = {
+  pagination
+};
