62dae96c00947b0e88c986d938fd0873b0985c60
Matías Ángel Dalceggio <matiasdalceggio@gmail.com>
2021-12-10 16:19:59 -0300

    controller and routes for pagination testimonials

--- routes/testimonials.js
+++ routes/testimonials.js
@@ -5,8 +5,9 @@ const router = express.Router();
 const authMiddleware = require('../middlewares/auth');
 const testimonialsMiddleware = require('../middlewares/testimonials');
 const testimonialsController = require('../controllers/testimonials');
+const paginationMiddleware = require('../middlewares/pagination');
 
-router.get('/', authMiddleware.isAdmin, testimonialsController.getAll);
+router.get('/', authMiddleware.isAdmin, paginationMiddleware.pageValidation, testimonialsController.getAll);
 router.get('/:id', authMiddleware.isAdmin, testimonialsController.getById);
 router.post('/', authMiddleware.isAdmin, testimonialsMiddleware.inputValidation, testimonialsController.create);
 router.put('/:id', authMiddleware.isAdmin, testimonialsController.update);
