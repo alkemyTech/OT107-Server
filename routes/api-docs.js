8da2bb324031b7d0e8ae3eb53b417886ab66f654
Andrea Maccan <acmaccan@gmail.com>
2021-12-10 20:52:57 -0300

    call to isAuth middleware deleted"

--- routes/api-docs.js
+++ routes/api-docs.js
@@ -3,8 +3,6 @@ const router = express.Router();
 const swaggerUI = require('swagger-ui-express');
 const swaggerSpecs = require('../modules/swagger');
 
-const authMiddleware = require('../middlewares/auth');
-
-router.use('/', authMiddleware.isAuth, swaggerUI.serve, swaggerUI.setup(swaggerSpecs));
+router.use('/', swaggerUI.serve, swaggerUI.setup(swaggerSpecs));
 
 module.exports = router;
