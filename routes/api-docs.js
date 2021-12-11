15415ddd8c0a812576c20ec5c72a3f064874e9b5
Andrea <78446973+acmaccan@users.noreply.github.com>
2021-12-11 20:32:10 -0300

    OT107-89: Swagger Module (#103)
    * Swagger Module
    
    * call to isAuth middleware deleted"
    
    * server line changed

---
+++ routes/api-docs.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const swaggerUI = require('swagger-ui-express');
+const swaggerSpecs = require('../modules/swagger');
+
+router.use('/', swaggerUI.serve, swaggerUI.setup(swaggerSpecs));
+
+module.exports = router;
