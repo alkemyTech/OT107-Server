655539686987f01351ce3b126d260ada95ab9eb2
fgarancini <f.garancini15@gmail.com>
2021-12-19 18:53:39 -0300

    OT107-101 Auth test

--- test/auth.js
+++ test/auth.js
@@ -23,6 +23,8 @@ describe('Post /auth/register', () => {
     password: 'testing@It59',
   };
 
+  let userID;
+
   const login = {
     email: 'fgaratests@gmail.com',
     password: 'testing@It59',
@@ -75,6 +77,91 @@ describe('Post /auth/register', () => {
           res.body.user.should.have.property('image');
           res.body.user.should.have.property('createdAt');
           res.body.user.should.have.property('updatedAt');
+          userID = res.body.user.id;
+          done();
+        });
+    });
+  });
+  describe('DELETE /user/:id', () => {
+    it('Shoud delete an user', (done) => {
+      chai
+        .request(app)
+        .delete(`/users/${userID}`)
+        .set({ Authorization: `Bearer ${token}` })
+        .end((err, res) => {
+          res.should.have.status(204);
+          done();
+        });
+    });
+  });
+});
+
+describe('Error cases route /auth', () => {
+  const userCase_password = {
+    firstName: 'Franco',
+    lastName: 'Garancini',
+    email: 'fgaratests@gmail.com',
+    image: 'bold.jpeg',
+    password: 'testingIt59',
+  };
+  const userCase_firstName = {
+    firstName: '',
+    lastName: 'Garancini',
+    email: 'fgaratests@gmail.com',
+    image: 'bold.jpeg',
+    password: 'testing@It59',
+  };
+
+  const userLogin = {
+    email: 'btc@test_com',
+    password: '123456',
+  };
+  describe('Route /auth/register', () => {
+    it('Should fail registration, wrong password format', (done) => {
+      chai
+        .request(app)
+        .post('/auth/register')
+        .send(userCase_password)
+        .end((err, res) => {
+          res.should.have.status(400);
+          res.body.error[0].should.be
+            .an('object')
+            .with.keys('value', 'msg', 'param', 'location');
+          res.body.error[0].msg.should.include('Invalid Password');
+          done();
+        });
+    });
+  });
+  describe('Route /auth/register', () => {
+    it('Should fail registration, First name required', (done) => {
+      chai
+        .request(app)
+        .post('/auth/register')
+        .send(userCase_firstName)
+        .end((err, res) => {
+          res.should.have.status(400);
+          res.body.error[0].should.be
+            .an('object')
+            .with.keys('value', 'msg', 'param', 'location');
+          res.body.error[0].msg.should.include('First Name Required');
+          done();
+        });
+    });
+  });
+
+  describe('Route /auth/login', () => {
+    it('Should fail login, Invalid Email', (done) => {
+      chai
+        .request(app)
+        .post('/auth/login')
+        .send(userLogin)
+        .end((err, res) => {
+          res.should.have.status(400);
+          res.body.error[0].should.be
+            .an('object')
+            .with.keys('value', 'msg', 'param', 'location');
+          res.body.error[0].msg.should.include('Invalid value');
+          res.body.error[0].param.should.include('email');
           done();
         });
     });
