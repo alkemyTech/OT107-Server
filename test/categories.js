333568a7218e025559907c759d6776b97f2fc64f
Sebastián Galván <sebastiangalvan.ar@gmail.com>
2021-12-19 21:00:10 -0300

    Ot107 107 - Test: Endpoints /categories [FIX]

--- test/categories.js
+++ test/categories.js
@@ -6,10 +6,15 @@ const app = require('../app');
 
 chai.should();
 chai.use(chaiHttp);
-let token;
+let tokenAdmin;
+let tokenStandard;
 
 const userAdmin = {
   email: 'agustin_tafura@test.com',
+  password: '123456'
+};
+const userStandard = {
+  email: 'e_musk@test.com',
   password: '123456'
 };
 
@@ -19,13 +24,23 @@ before((done) => {
     .send(userAdmin)
     .end((err, res) => {
       res.should.have.status(200);
-      token = res.body.token;
+      tokenAdmin = res.body.token;
+      done();
+    });
+});
+before((done) => {
+  chai.request(app)
+    .post('/auth/login')
+    .send(userStandard)
+    .end((err, res) => {
+      res.should.have.status(200);
+      tokenStandard = res.body.token;
       done();
     });
 });
 
 describe('Category Tests', () => {
-  describe('GET /categories', () => {
+  describe('GET /categories isAuth [fakeToken]', () => {
     it('Invalid Token', (done) => {
       chai.request(app)
         .get('/categories')
@@ -35,10 +50,10 @@ describe('Category Tests', () => {
           done();
         });
     });
-    it('it should get the categories data', (done) => {
+    it('GET /categories isAuth [Admin]', (done) => {
       chai.request(app)
         .get('/categories')
-        .set({ Authorization: `Bearer ${token}` })
+        .set({ Authorization: `Bearer ${tokenAdmin}` })
         .end((err, res) => {
           res.should.have.status(200);
           res.should.be.a('object');
@@ -50,58 +65,73 @@ describe('Category Tests', () => {
           done();
         });
     });
-    it('Category data should not be retrieved, categorie does not exist.', (done) => {
+    it('GET /categories isAuth [Standard]', (done) => {
+      chai.request(app)
+        .get('/categories')
+        .set({ Authorization: `Bearer ${tokenStandard}` })
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.should.be.a('object');
+          res.body.should.have.property('count');
+          res.body.should.have.property('lastPage');
+          res.body.should.have.property('previousPage');
+          res.body.should.have.property('nextPage');
+          res.body.should.have.property('data');
+          done();
+        });
+    });
+    it('GET /categorie, categorie does not exist.', (done) => {
       chai.request(app)
         .get('/categorie')
-        .set({ Authorization: `Bearer ${token}` })
+        .set({ Authorization: `Bearer ${tokenAdmin}` })
         .end((err, res) => {
           res.should.have.status(404);
           done();
         });
     });
-    it('You should get the category data on page 3.', (done) => {
+    it('GET /categories?page=3.', (done) => {
       chai.request(app)
         .get('/categories?page=3')
-        .set({ Authorization: `Bearer ${token}` })
+        .set({ Authorization: `Bearer ${tokenStandard}` })
         .end((err, res) => {
           res.should.have.status(200);
           done();
         });
     });
-    it('The page parameter must be a number.', (done) => {
+    it('GET /categories?page=h.', (done) => {
       chai.request(app)
         .get('/categories?page=h')
-        .set({ Authorization: `Bearer ${token}` })
+        .set({ Authorization: `Bearer ${tokenStandard}` })
         .end((err, res) => {
           res.text.should.include('The page parameter must be a number.');
           res.should.have.status(400);
           done();
         });
     });
-    it('You must provide a page number.', (done) => {
+    it('GET /categories?page=.', (done) => {
       chai.request(app)
         .get('/categories?page=')
-        .set({ Authorization: `Bearer ${token}` })
+        .set({ Authorization: `Bearer ${tokenStandard}` })
         .end((err, res) => {
           res.text.should.include('You must provide a page number.');
           res.should.have.status(400);
           done();
         });
     });
-    it('The page must be greater than one.', (done) => {
+    it('GET /categories?page=-5.', (done) => {
       chai.request(app)
         .get('/categories?page=-5')
-        .set({ Authorization: `Bearer ${token}` })
+        .set({ Authorization: `Bearer ${tokenStandard}` })
         .end((err, res) => {
           res.text.should.include('The page must be greater than one.');
           res.should.have.status(400);
           done();
         });
     });
-    it('The requested page is greater than the last page.', (done) => {
+    it('GET /categories?page=300.', (done) => {
       chai.request(app)
         .get('/categories?page=300')
-        .set({ Authorization: `Bearer ${token}` })
+        .set({ Authorization: `Bearer ${tokenStandard}` })
         .end((err, res) => {
           res.text.should.include('The requested page is greater than the last page.');
           res.should.have.status(400);
@@ -109,11 +139,11 @@ describe('Category Tests', () => {
         });
     });
 
-    describe('GET categories/:id  id = 1', () => {
-      it('Should get the category whith id = 1', (done) => {
+    describe('GET categories/:id', () => {
+      it('GET categories/:id isAdmin [Admin] id = 1', (done) => {
         chai.request(app)
           .get('/categories/1')
-          .set({ Authorization: `Bearer ${token}` })
+          .set({ Authorization: `Bearer ${tokenAdmin}` })
           .end((err, res) => {
             res.should.have.status(200);
             res.body.should.be.a('object');
@@ -124,10 +154,28 @@ describe('Category Tests', () => {
             done();
           });
       });
-      it("Should't get the category whith id = g", (done) => {
+      it('GET categories/:id isAdmin [Standar] id = 1', (done) => {
+        chai.request(app)
+          .get('/categories/1')
+          .set({ Authorization: `Bearer ${tokenStandard}` })
+          .end((err, res) => {
+            res.should.have.status(401);
+            done();
+          });
+      });
+      it('GET categories/:id isAdmin [fakeToken] id = 1', (done) => {
+        chai.request(app)
+          .get('/categories/1')
+          .set({ Authorization: 'Bearer fakeToken' })
+          .end((err, res) => {
+            res.should.have.status(400);
+            done();
+          });
+      });
+      it('GET categories/:id isAdmin [Admin] id = g', (done) => {
         chai.request(app)
           .get('/categories/g')
-          .set({ Authorization: `Bearer ${token}` })
+          .set({ Authorization: `Bearer ${tokenAdmin}` })
           .end((err, res) => {
             res.should.have.status(404);
             done();
@@ -137,10 +185,10 @@ describe('Category Tests', () => {
   });
 
   describe('POST /categories', () => {
-    it('Should insert a category', (done) => {
+    it('POST /categories isAdmin [Admin]', (done) => {
       chai.request(app)
         .post('/categories')
-        .set({ Authorization: `Bearer ${token}` })
+        .set({ Authorization: `Bearer ${tokenAdmin}` })
         .send({
           name: 'Nueva Categoria Test',
           description: 'Description de una nueva categoria',
@@ -153,10 +201,42 @@ describe('Category Tests', () => {
           done();
         });
     });
-    it('The name parameter is required', (done) => {
+    it('POST /categories isAdmin [Standar]', (done) => {
       chai.request(app)
         .post('/categories')
-        .set({ Authorization: `Bearer ${token}` })
+        .set({ Authorization: `Bearer ${tokenStandard}` })
+        .send({
+          name: 'Nueva Categoria Test',
+          description: 'Description de una nueva categoria',
+          image: 'image21.jpg',
+          updatedAt: new Date(),
+          createdAt: new Date(),
+        })
+        .end((err, res) => {
+          res.should.have.status(401);
+          done();
+        });
+    });
+    it('POST /categories isAdmin [fakeToken]', (done) => {
+      chai.request(app)
+        .post('/categories')
+        .set({ Authorization: 'Bearer faketoken' })
+        .send({
+          name: 'Nueva Categoria Test',
+          description: 'Description de una nueva categoria',
+          image: 'image21.jpg',
+          updatedAt: new Date(),
+          createdAt: new Date(),
+        })
+        .end((err, res) => {
+          res.should.have.status(400);
+          done();
+        });
+    });
+    it('POST /categories name is required', (done) => {
+      chai.request(app)
+        .post('/categories')
+        .set({ Authorization: `Bearer ${tokenAdmin}` })
         .send({
           name: '',
           description: 'Description de una nueva categoria',
@@ -169,10 +249,10 @@ describe('Category Tests', () => {
           done();
         });
     });
-    it('It should not be registered, the name must be a string', (done) => {
+    it('POST /categories, the name must be a string', (done) => {
       chai.request(app)
         .post('/categories')
-        .set({ Authorization: `Bearer ${token}` })
+        .set({ Authorization: `Bearer ${tokenAdmin}` })
         .send({
           name: 45421,
           description: 'Description de una nueva categoria',
@@ -185,10 +265,10 @@ describe('Category Tests', () => {
           done();
         });
     });
-    it('It should not be registered, the name must be a string', (done) => {
+    it('POST /categories, the name must be a string', (done) => {
       chai.request(app)
         .post('/categories')
-        .set({ Authorization: `Bearer ${token}` })
+        .set({ Authorization: `Bearer ${tokenAdmin}` })
         .send({
           name: false,
           description: 'Description de una nueva categoria',
@@ -201,10 +281,10 @@ describe('Category Tests', () => {
           done();
         });
     });
-    it('It should not be registered, the name must be a string', (done) => {
+    it('POST /categories, the name must be a string', (done) => {
       chai.request(app)
         .post('/categories')
-        .set({ Authorization: `Bearer ${token}` })
+        .set({ Authorization: `Bearer ${tokenAdmin}` })
         .send({
           name: [],
           description: 'Description de una nueva categoria',
@@ -219,11 +299,11 @@ describe('Category Tests', () => {
     });
   });
 
-  describe('UPDATE the name of category with ID = 2', () => {
-    it('Should update the name of category', (done) => {
+  describe('PUT /categories isAdmin', () => {
+    it('PUT /categories/2 [Admin] update the name', (done) => {
       chai.request(app)
         .put('/categories/2')
-        .set({ Authorization: `Bearer ${token}` })
+        .set({ Authorization: `Bearer ${tokenAdmin}` })
         .send({ name: 'Update Categoria' })
         .end((err, res) => {
           res.body.should.have.property('id').to.be.equal(2);
@@ -232,70 +312,120 @@ describe('Category Tests', () => {
           done();
         });
     });
-    it("Should't update category, the id parameter must be a number.", (done) => {
+    it('PUT /categories/2 [Standar] update the name', (done) => {
+      chai.request(app)
+        .put('/categories/2')
+        .set({ Authorization: `Bearer ${tokenStandard}` })
+        .send({ name: 'Update Categoria' })
+        .end((err, res) => {
+          res.should.have.status(401);
+          done();
+        });
+    });
+    it('PUT /categories/2 [fakeToken] update the name', (done) => {
+      chai.request(app)
+        .put('/categories/2')
+        .set({ Authorization: 'Bearer fakeToken' })
+        .send({ name: 'Update Categoria' })
+        .end((err, res) => {
+          res.should.have.status(400);
+          done();
+        });
+    });
+    it('PUT /categories/2 [Admin] update the description', (done) => {
+      chai.request(app)
+        .put('/categories/2')
+        .set({ Authorization: `Bearer ${tokenAdmin}` })
+        .send({
+          name: 'Update Categoria',
+          description: 'Una descripcion'
+        })
+        .end((err, res) => {
+          res.body.should.have.property('id').to.be.equal(2);
+          res.body.should.have.property('name').to.be.equal('Update Categoria');
+          res.should.have.status(200);
+          done();
+        });
+    });
+    it('PUT /categories/2 [Admin] update the image', (done) => {
+      chai.request(app)
+        .put('/categories/2')
+        .set({ Authorization: `Bearer ${tokenAdmin}` })
+        .send({
+          name: 'Update Categoria',
+          image: 'imagen.jpg'
+        })
+        .end((err, res) => {
+          res.body.should.have.property('id').to.be.equal(2);
+          res.body.should.have.property('name').to.be.equal('Update Categoria');
+          res.should.have.status(200);
+          done();
+        });
+    });
+    it('PUT /categories/k, the id parameter must be a number.', (done) => {
       chai.request(app)
         .put('/categories/k')
-        .set({ Authorization: `Bearer ${token}` })
+        .set({ Authorization: `Bearer ${tokenAdmin}` })
         .send({ name: 'Update Categoria' })
         .end((err, res) => {
           res.should.have.status(404);
           done();
         });
     });
-    it("It shouldn't update, the category name is required.", (done) => {
+    it('PUT /categories/2 the category name is required.', (done) => {
       chai.request(app)
         .put('/categories/2')
-        .set({ Authorization: `Bearer ${token}` })
+        .set({ Authorization: `Bearer ${tokenAdmin}` })
         .send({ name: '' })
         .end((err, res) => {
           res.should.have.status(401);
           done();
         });
     });
-    it('The id parameter :id is required.', (done) => {
+    it('PUT /categories/, :id is required.', (done) => {
       chai.request(app)
         .put('/categories/')
-        .set({ Authorization: `Bearer ${token}` })
+        .set({ Authorization: `Bearer ${tokenAdmin}` })
         .send({ name: 'New Category' })
         .end((err, res) => {
           res.should.have.status(404);
           done();
         });
     });
-    it('It should not update, id category does not exist.', (done) => {
+    it('PUT /categories/456, :id category does not exist.', (done) => {
       chai.request(app)
         .put('/categories/456')
-        .set({ Authorization: `Bearer ${token}` })
+        .set({ Authorization: `Bearer ${tokenAdmin}` })
         .send({ name: 'New Category' })
         .end((err, res) => {
           res.should.have.status(404);
           done();
         });
     });
-    it("It shouldn't update, the name must be a String.", (done) => {
+    it('PUT /categories/2, the name must be a String.', (done) => {
       chai.request(app)
         .put('/categories/2')
-        .set({ Authorization: `Bearer ${token}` })
+        .set({ Authorization: `Bearer ${tokenAdmin}` })
         .send({ name: 13245 })
         .end((err, res) => {
           res.should.have.status(401);
           done();
         });
     });
-    it("It shouldn't update, the name must be a String.", (done) => {
+    it('PUT /categories/2, the name must be a String.', (done) => {
       chai.request(app)
         .put('/categories/2')
-        .set({ Authorization: `Bearer ${token}` })
+        .set({ Authorization: `Bearer ${tokenAdmin}` })
         .send({ name: true })
         .end((err, res) => {
           res.should.have.status(401);
           done();
         });
     });
-    it("It shouldn't update, the name must be a String.", (done) => {
+    it('PUT /categories/2, the name must be a String.', (done) => {
       chai.request(app)
         .put('/categories/2')
-        .set({ Authorization: `Bearer ${token}` })
+        .set({ Authorization: `Bearer ${tokenAdmin}` })
         .send({ name: [] })
         .end((err, res) => {
           res.should.have.status(401);
@@ -304,38 +434,56 @@ describe('Category Tests', () => {
     });
   });
 
-  describe('DELETE /categories', () => {
-    it('Should delete the category with id = 11', (done) => {
+  describe('DELETE /categories/:id isAdmin', () => {
+    it('DELETE /categories/11 [Admin]', (done) => {
       chai.request(app)
         .delete('/categories/11')
-        .set({ Authorization: `Bearer ${token}` })
+        .set({ Authorization: `Bearer ${tokenAdmin}` })
         .end((err, response) => {
           response.should.have.status(200);
           done();
         });
     });
-    it('You cannot delete a category that is out of range.', (done) => {
+    it('DELETE /categories/11 [Standard]', (done) => {
+      chai.request(app)
+        .delete('/categories/11')
+        .set({ Authorization: `Bearer ${tokenStandard}` })
+        .end((err, response) => {
+          response.should.have.status(401);
+          done();
+        });
+    });
+    it('DELETE /categories/11 [fakeToken]', (done) => {
+      chai.request(app)
+        .delete('/categories/11')
+        .set({ Authorization: 'Bearer fakeToken' })
+        .end((err, response) => {
+          response.should.have.status(400);
+          done();
+        });
+    });
+    it('DELETE /categories/448', (done) => {
       chai.request(app)
         .delete('/categories/448')
-        .set({ Authorization: `Bearer ${token}` })
+        .set({ Authorization: `Bearer ${tokenAdmin}` })
         .end((err, response) => {
           response.should.have.status(404);
           done();
         });
     });
-    it('The id parameter :id is required.', (done) => {
+    it('DELETE /categories/, :id is required.', (done) => {
       chai.request(app)
         .delete('/categories/')
-        .set({ Authorization: `Bearer ${token}` })
+        .set({ Authorization: `Bearer ${tokenAdmin}` })
         .end((err, response) => {
           response.should.have.status(404);
           done();
         });
     });
-    it('It should not be removed, it has an associated novelty', (done) => {
+    it('DELETE /categories/6, It should not be removed, it has an associated novelty', (done) => {
       chai.request(app)
         .delete('/categories/6')
-        .set({ Authorization: `Bearer ${token}` })
+        .set({ Authorization: `Bearer ${tokenAdmin}` })
         .end((err, response) => {
           response.should.have.status(401);
           done();
