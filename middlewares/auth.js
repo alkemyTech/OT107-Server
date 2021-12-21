60c127dfbe032f81f9ff16ef103341e3d2564e72
Andrea Maccan <acmaccan@gmail.com>
2021-12-21 17:45:41 -0300

    isAuth errors middlewares

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -43,18 +43,18 @@ const isAdmin = async (req, res, next) => {
 
 const isAuth = async (req, res, next) => {
   const bearertoken = req.headers.authorization;
-  if (!bearertoken) return res.status(401).json({ message: 'Access denied' });
+  if (!bearertoken) return res.status(401).json({ error: 'Access denied' });
 
   try {
     const token = bearertoken.split(' ')[1];
     const { id } = auth.decodeToken(token);
     const userAuth = await usersServices.getById(id);
     req.params.tokenizedUserId = userAuth.id;
-    if (!userAuth) return res.status(401).json({ message: 'El usuario no existe' });
+    if (!userAuth) return res.status(401).json({ error: 'Access denied' });
 
     next();
   } catch (error) {
-    res.status(401).json(`Invalid token - ${error.message}`);
+    res.status(401).json({ error: 'Access denied' });
   }
 };
 
