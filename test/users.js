6fa8d9774b597ed08e967ce19cef4668fb9f8c06
fgarancini <f.garancini15@gmail.com>
2021-12-22 18:22:56 -0300

    Format and cases

--- test/users.js
+++ test/users.js
@@ -8,6 +8,7 @@ chai.should();
 chai.use(chaihttp);
 
 let token;
+let tokenDelete;
 
 describe('Route /users', () => {
   const user = {
@@ -26,21 +27,19 @@ describe('Route /users', () => {
       });
   });
 
-  describe('GET /users', () => {
-    it('It should get all the users', (done) => {
-      chai
-        .request(app)
-        .get('/users')
-        .set({ Authorization: `Bearer ${token}` })
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.should.be.an('Object');
-          res.body[0].should.have.property('firstName');
-          res.body[0].should.have.property('email');
-          res.body[0].should.have.property('image');
-          done();
-        });
-    });
+  it('It should get all the users', (done) => {
+    chai
+      .request(app)
+      .get('/users')
+      .set({ Authorization: `Bearer ${token}` })
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.should.be.an('Object');
+        res.body[0].should.have.property('firstName');
+        res.body[0].should.have.property('email');
+        res.body[0].should.have.property('image');
+        done();
+      });
   });
 });
 
@@ -60,36 +59,23 @@ describe('Route /users/:id', () => {
         done();
       });
   });
-  describe('PATCH /users/:id', () => {
-    it('It should update an user', (done) => {
-      const update = { firstname: 'Vitalik', lastname: 'Vutlerin' };
-      chai
-        .request(app)
-        .patch('/users/19')
-        .send(update)
-        .set({ Authorization: `Bearer ${token}` })
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.should.be.an('Object');
-          res.body.user.should.have.property('firstName');
-          res.body.user.should.have.property('email');
-          res.body.user.should.have.property('image');
-          done();
-        });
-    });
+
+  it('It should update an user', (done) => {
+    const update = { firstname: 'Vitalik', lastname: 'Vutlerin' };
+    chai
+      .request(app)
+      .patch('/users/19')
+      .send(update)
+      .set({ Authorization: `Bearer ${token}` })
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.should.be.an('Object');
+        res.body.user.should.have.property('firstName');
+        res.body.user.should.have.property('email');
+        res.body.user.should.have.property('image');
+        done();
+      });
   });
-  // describe("DELETE /user/:id", () => {
-  //   it("Shoud delete an user", (done) => {
-  //     chai
-  //       .request(app)
-  //       .delete("/users/17")
-  //       .set({ Authorization: `Bearer ${token}` })
-  //       .end((err, res) => {
-  //         res.should.have.status(204);
-  //         done();
-  //       });
-  //   });
-  // });
 });
 
 describe('Route /users/:id', () => {
@@ -109,45 +95,67 @@ describe('Route /users/:id', () => {
       });
   });
 
-  describe('Route /users', () => {
-    it('should not get the list of users', (done) => {
-      chai
-        .request(app)
-        .get('/users')
-        .set({ Authorization: `Bearer ${token}` })
-        .end((err, res) => {
-          res.should.have.status(401);
-          done();
-        });
-    });
+  it('should not get the list of users', (done) => {
+    chai
+      .request(app)
+      .get('/users')
+      .set({ Authorization: `Bearer ${token}` })
+      .end((err, res) => {
+        res.should.have.status(401);
+        done();
+      });
   });
 
-  describe('Route /users', () => {
-    it('should not update the user', (done) => {
-      const update = { firstname: 'Vitalik', lastname: 'Vutlerin' };
+  it('should not update the user', (done) => {
+    const update = { firstname: 'Vitalik', lastname: 'Vutlerin' };
 
-      chai
-        .request(app)
-        .patch('/users/13')
-        .send(update)
-        .set({ Authorization: `Bearer ${token}` })
-        .end((err, res) => {
-          res.should.have.status(400);
-          done();
-        });
-    });
+    chai
+      .request(app)
+      .patch('/users/13')
+      .send(update)
+      .set({ Authorization: `Bearer ${token}` })
+      .end((err, res) => {
+        res.should.have.status(400);
+        done();
+      });
   });
 
-  describe('Route /users', () => {
-    it('should not delete the user', (done) => {
-      chai
-        .request(app)
-        .delete('/users/13')
-        .set({ Authorization: `Bearer ${token}` })
-        .end((err, res) => {
-          res.should.have.status(400);
-          done();
-        });
-    });
+  it('should not delete the user', (done) => {
+    chai
+      .request(app)
+      .delete('/users/13')
+      .set({ Authorization: `Bearer ${token}` })
+      .end((err, res) => {
+        res.should.have.status(400);
+        done();
+      });
+  });
+});
+
+describe('DELETE /user/:id', () => {
+  const user = {
+    email: 'lana_trico@gmail.com',
+    password: '123456@Miau',
+  };
+  before((done) => {
+    chai
+      .request(app)
+      .post('/auth/login')
+      .send(user)
+      .end((err, res) => {
+        res.should.have.status(200);
+        tokenDelete = res.body.token;
+        done();
+      });
+  });
+  it('Shoud delete an user', (done) => {
+    chai
+      .request(app)
+      .delete('/users/19')
+      .set({ Authorization: `Bearer ${tokenDelete}` })
+      .end((err, res) => {
+        res.should.have.status(204);
+        done();
+      });
   });
 });
