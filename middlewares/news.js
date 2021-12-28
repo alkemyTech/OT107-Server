c1862dee42878204361200f1b1b0288063fd8ee1
AgustinTafura <agustintafura@hotmail.com>
2021-12-28 17:31:42 -0300

    update image validation

--- middlewares/news.js
+++ middlewares/news.js
@@ -4,13 +4,19 @@ const filesModule = require('../modules/filesHandler');
 const newsInputValidation = [
   check('name').exists().notEmpty().isLength({ min: 3 }),
   check('content').exists().notEmpty().isLength({ min: 3 }),
-  check('image').exists().notEmpty().custom((value) => {
-    filesModule.isImage(value);
-    return true;
-  }),
   check('categoryId').exists().notEmpty().isNumeric(),
   (req, res, next) => {
     const errors = validationResult(req);
+    const file = req.files.image;
+    if (!filesModule.isImage(file.name)) {
+      errors.errors.push(
+        {
+          msg: 'Invalid image extension',
+          param: 'image',
+          location: 'files'
+        }
+      );
+    }
     if (!errors.isEmpty()) {
       return res.status(400).json({ error: errors.array() });
     }
